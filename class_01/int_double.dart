725208403a7bb55ab394509c4ffe23eb67cf145b
ambanikousik <rikousik@gmail.com>
2021-10-03 12:13:06 +0600

    Create int_double.dart

---
+++ class_01/int_double.dart
@@ -0,0 +1,19 @@
+void main() {
+  int apple = 5;
+  int price = 51;
+  
+  late int x;
+  
+  
+  x = apple * price;
+  
+  print("total price: ${apple+price}");
+  
+  
+  double a = 24.6;
+  double b = 31.2;
+  double totalDouble = a+b;
+  
+    print("total double: $totalDouble");
+   
+ }
