56b49365c04c6148f7dddff6411448a65445520d
ambanikousik <rikousik@gmail.com>
2021-10-03 10:58:11 +0600

    Create if_else.dart

---
+++ class_01/if_else.dart
@@ -0,0 +1,18 @@
+void main() {
+String weather = "summer";
+ int temparature = 20;
+  
+  if(weather == 'summer' ||  temparature > 25){
+    print('I will drink cold 
+    water');
+  }else if(weather == 'rainy'){
+    
+    print('I will take an umbrella');
+  }
+  
+           else if(weather == 'storm'){
+              print('I will not go out');
+           }else{
+             print('I will go out');
+           }
+}
